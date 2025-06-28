070ae843e69b015fb89f31849bc9132a953fc017
nusnug <stevesun5160@gmail.com>
2025-06-28 12:29:43 +0800

    feat:add server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,121 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import typeDefs from './schema/index.js';
-import resolvers from './resolvers/index.js';
+
+const typeDefs = `#graphql
+  type Detail {
+    boxOffice: String!
+    duration: Int!
+  }
+
+  type Movie {
+    id: ID!
+    title: String!
+    director: Director!
+    releaseYear: Int!
+    genre: String!
+    img: String!
+    detail: Detail!
+  }
+
+  type Director {
+    name: String!
+    age: Int!
+    gender: String!
+  }
+
+  type Query {
+    allMovies: [Movie!]!
+    allGenres: [String!]!
+    allDirectors: [Director!]!
+    movieDetail(id: ID!): Detail!
+  }
+`;
+
+// Fake data
+const genreList = {
+  FICTION: '虛構',
+  NON_FICTION: '非虛構',
+  MYSTERY: '懸疑',
+  FANTASY: '奇幻',
+  SCIENCE_FICTION: '科幻',
+} as const;
+
+const directors = [
+  { name: '克里斯多福·諾蘭', age: 52, gender: '男' },
+  { name: '葛莉塔·葛薇', age: 40, gender: '女' },
+  { name: '史蒂芬·史匹柏', age: 77, gender: '男' },
+  { name: '趙婷', age: 41, gender: '女' },
+  { name: '昆汀·塔倫提諾', age: 61, gender: '男' },
+  { name: '丹尼斯·維勒納夫', age: 56, gender: '男' },
+];
+
+const movies = [
+  {
+    id: '1',
+    title: '全面啟動',
+    director: directors[0],
+    releaseYear: 2010,
+    genre: '虛構',
+    img: 'https://upload.wikimedia.org/wikipedia/zh/7/7f/Inception_ver3.jpg',
+    detail: { boxOffice: '8.36億美元', duration: 148 },
+  },
+  {
+    id: '2',
+    title: '她們',
+    director: directors[1],
+    releaseYear: 2019,
+    genre: '虛構',
+    img: 'https://upload.wikimedia.org/wikipedia/zh/c/c9/Little_Women_%282019_film%29.jpg',
+    detail: { boxOffice: '2.18億美元', duration: 135 },
+  },
+  {
+    id: '3',
+    title: '侏羅紀公園',
+    director: directors[2],
+    releaseYear: 1993,
+    genre: '奇幻',
+    img: 'https://upload.wikimedia.org/wikipedia/zh/e/e7/Jurassic_Park_poster.jpg',
+    detail: { boxOffice: '10.46億美元', duration: 127 },
+  },
+  {
+    id: '4',
+    title: '游牧人生',
+    director: directors[3],
+    releaseYear: 2020,
+    genre: '非虛構',
+    img: 'https://upload.wikimedia.org/wikipedia/zh/f/f0/Nomadland.jpg',
+    detail: { boxOffice: '3900萬美元', duration: 108 },
+  },
+  {
+    id: '5',
+    title: '黑色追緝令',
+    director: directors[4],
+    releaseYear: 1994,
+    genre: '懸疑',
+    img: 'https://upload.wikimedia.org/wikipedia/zh/8/82/Pulp_Fiction_cover.jpg',
+    detail: { boxOffice: '2.13億美元', duration: 154 },
+  },
+  {
+    id: '6',
+    title: '沙丘',
+    director: directors[5],
+    releaseYear: 2020,
+    genre: '科幻',
+    img: 'https://upload.wikimedia.org/wikipedia/zh/7/76/Dune_%282021_film%29_poster.jpg',
+    detail: { boxOffice: '4.02億美元', duration: 155 },
+  },
+];
+
+const resolvers = {
+  Query: {
+    allMovies: () => movies,
+    allGenres: () => Object.values(genreList),
+    allDirectors: () => directors,
+    movieDetail: (_: any, { id }: { id: string }) => {
+      return movies.find((movie) => movie.id === id).detail;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
@@ -9,7 +123,7 @@ const server = new ApolloServer({
 });
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: 5171 },
 });
 
-console.log(`🚀 Server ready at: ${url}`);
+console.log(`🚀  Server ready at: ${url}`);
